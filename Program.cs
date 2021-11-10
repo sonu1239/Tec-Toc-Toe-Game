a7321002738da021e86c022b1e354eda4e2011d2
sonu1239 <ss316742@gmail.com>
2021-11-10 13:11:28 +0530

    tossing to select first turn

--- Program.cs
+++ Program.cs
@@ -9,13 +9,25 @@ namespace Tic_Toc_Toe_Game
 {
 	class Program
 	{
-	
+
+		static char[] board = new char[10];
 		public static void Main(String[] args)
 		{
 			Console.WriteLine("welcome to Tic Tac Toi GAME");
-			Console.ReadLine();
+			Board();                            //calling the method board in main method	
 		}
-		
-		
+		public static void Board()
+		{
+			// giving each array index value as empty initially
+			for (int i = 1; i < 10; i++)
+			{
+				board[i] = ' ';	
+
+				Console.ReadLine();
+
+
+
+			}
+		}	
 	}
 }
